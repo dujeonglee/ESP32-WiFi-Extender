ea01f23318ea841033df51e913d957d66def85ae
dujeonglee <dujeong.lee82@gmail.com>
2020-02-15 00:34:35 +0900

    Implement broadcast and multicast chain/
    Signed-off-by: dujeonglee <dujeong.lee82@gmail.com>

---
+++ main/bcmc_chain.h
@@ -0,0 +1,18 @@
+/**
+ * @file bcmc_chain.h
+ * @author Dujeong Lee (dujeong.lee82@gmail.com)
+ * @brief Broadcast Multicast chain
+ * @version 0.1
+ * @date 2020-02-14
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+#ifndef BCMCCHAIN_H
+#define BCMCCHAIN_H
+#include "common.h"
+
+bool bcmc_filter(const tcpip_adapter_if_t type, struct pbuf *p);
+pkt_fate_t bcmc_process_ap(const tcpip_adapter_if_t type, struct pbuf *p);
+pkt_fate_t bcmc_process_sta(const tcpip_adapter_if_t type, struct pbuf *p);
+#endif
